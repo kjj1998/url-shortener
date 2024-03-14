b0468c26ad6124fc0e9d006ddec23825e6d59d98
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-03-14 22:47:30 +0800

    optimized Dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-poetry run alembic upgrade heads
+# poetry run alembic upgrade heads
 
-poetry run uvicorn url_shortener.web.app:app --host 0.0.0.0 --port 8000 --reload
+# poetry run uvicorn url_shortener.web.app:app --host 0.0.0.0 --port 8000 --reload
+
+alembic upgrade heads
+uvicorn url_shortener.web.app:app --host 0.0.0.0 --port 8000 --reload
