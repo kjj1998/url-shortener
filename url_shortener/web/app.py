f52019293e311984b077c59b4ff92dd99be8ec17
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-04-20 15:29:33 +0800

    changed api endpoints and added url-shortener-auth for eks deployment

--- url_shortener/web/app.py
+++ url_shortener/web/app.py
@@ -6,7 +6,11 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from url_shortener.web.api.api import router as url_shortener_router
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/url-shortener/docs",
+    redoc_url=None,
+    openapi_url="/url-shortener/openapi.json",
+)
 
 origins = ["http://127.0.0.1:8000"]
 
@@ -18,10 +22,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(url_shortener_router, tags=["url_shortener"])
+app.include_router(
+    url_shortener_router, prefix="/url-shortener", tags=["url_shortener"]
+)
 
 
-@app.get("/")
+@app.get("/url-shortener")
 async def root():
     """Root endpoint."""
     return {"message": f"Hello from {os.getenv("HOSTNAME", default="local")}"}
