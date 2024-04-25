da14f41e7d59b193c97992f57f426a0c43644b39
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-04-25 23:12:04 +0800

    modularize aws vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,179 +12,194 @@ provider "aws" {
   profile = "admin-1"
 }
 
-# VPC
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "url-shortener-cluster-vpc-iac"
-  }
+module "vpc" {
+  source = "./modules/aws-vpc"
+  vpc_name = "url-shortener-cluster-vpc-iac"
+  public_subnet_1_name = "url-shortener-cluster-vpc-subnet-public1-ap-southeast-1a-iac"
+  public_subnet_2_name = "url-shortener-cluster-vpc-subnet-public2-ap-southeast-1b-iac"
+  public_subnet_3_name = "url-shortener-cluster-vpc-subnet-public3-ap-southeast-1c-iac"
+  private_subnet_1_name = "url-shortener-cluster-vpc-subnet-private1-ap-southeast-1a-iac"
+  private_subnet_2_name = "url-shortener-cluster-vpc-subnet-private2-ap-southeast-1b-iac"
+  private_subnet_3_name = "url-shortener-cluster-vpc-subnet-private3-ap-southeast-1c-iac"
+  internet_gateway_name = "url-shortener-cluster-vpc-igw-iac"
+  nat_gateway_name = "url-shortener-nat-gateway"
+  public_subnet_route_table_name = "url-shortener-cluster-vpc-rtb-public-iac"
+  private_subnet_route_table_name = "url-shortener-cluster-vpc-rtb-private-iac"
 }
+
+# VPC
+# resource "aws_vpc" "vpc" {
+#   cidr_block           = "10.0.0.0/16"
+#   instance_tenancy     = "default"
+#   enable_dns_hostnames = true
+
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-iac"
+#   }
+# }
 
 # Public subnets
-resource "aws_subnet" "public_subnet_1" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = "ap-southeast-1a"
+# resource "aws_subnet" "public_subnet_1" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.0.0/19"
+#   availability_zone = "ap-southeast-1a"
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-subnet-public1-ap-southeast-1a-iac"
-  }
-}
-resource "aws_subnet" "public_subnet_2" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.32.0/19"
-  availability_zone = "ap-southeast-1b"
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-subnet-public1-ap-southeast-1a-iac"
+#   }
+# }
+# resource "aws_subnet" "public_subnet_2" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.32.0/19"
+#   availability_zone = "ap-southeast-1b"
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-subnet-public2-ap-southeast-1b-iac"
-  }
-}
-resource "aws_subnet" "public_subnet_3" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.64.0/19"
-  availability_zone = "ap-southeast-1c"
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-subnet-public2-ap-southeast-1b-iac"
+#   }
+# }
+# resource "aws_subnet" "public_subnet_3" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.64.0/19"
+#   availability_zone = "ap-southeast-1c"
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-subnet-public3-ap-southeast-1c-iac"
-  }
-}
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-subnet-public3-ap-southeast-1c-iac"
+#   }
+# }
 
 # Private subnets
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.128.0/19"
-  availability_zone = "ap-southeast-1a"
+# resource "aws_subnet" "private_subnet_1" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.128.0/19"
+#   availability_zone = "ap-southeast-1a"
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-subnet-private1-ap-southeast-1a-iac"
-  }
-}
-resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.160.0/19"
-  availability_zone = "ap-southeast-1b"
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-subnet-private1-ap-southeast-1a-iac"
+#   }
+# }
+# resource "aws_subnet" "private_subnet_2" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.160.0/19"
+#   availability_zone = "ap-southeast-1b"
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-subnet-private2-ap-southeast-1b-iac"
-  }
-}
-resource "aws_subnet" "private_subnet_3" {
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.192.0/19"
-  availability_zone = "ap-southeast-1c"
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-subnet-private2-ap-southeast-1b-iac"
+#   }
+# }
+# resource "aws_subnet" "private_subnet_3" {
+#   vpc_id            = aws_vpc.vpc.id
+#   cidr_block        = "10.0.192.0/19"
+#   availability_zone = "ap-southeast-1c"
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-subnet-private3-ap-southeast-1c-iac"
-  }
-}
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-subnet-private3-ap-southeast-1c-iac"
+#   }
+# }
 
 
 # Internet gateway
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.vpc.id
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-igw-iac"
-  }
-}
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-igw-iac"
+#   }
+# }
 
-# Elastic IPs
-resource "aws_eip" "nat_gateway_1_eip" {
-  domain = "vpc"
-}
+# # Elastic IPs
+# resource "aws_eip" "nat_gateway_1_eip" {
+#   domain = "vpc"
+# }
 
-# Nat gateways
-resource "aws_nat_gateway" "private_subnet_1_nat_gateway" {
-  connectivity_type = "public"
-  subnet_id = aws_subnet.private_subnet_1.id
-  allocation_id = aws_eip.nat_gateway_1_eip.id
+# # Nat gateways
+# resource "aws_nat_gateway" "private_subnet_1_nat_gateway" {
+#   connectivity_type = "public"
+#   subnet_id = aws_subnet.private_subnet_1.id
+#   allocation_id = aws_eip.nat_gateway_1_eip.id
 
-  tags = {
-    Name = "url-shortener-nat-gateway-1"
-  }
+#   tags = {
+#     Name = "url-shortener-nat-gateway-1"
+#   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
-  depends_on = [aws_internet_gateway.igw]
-}
+#   # To ensure proper ordering, it is recommended to add an explicit dependency
+#   # on the Internet Gateway for the VPC.
+#   depends_on = [aws_internet_gateway.igw]
+# }
 
-# Public subnet route table
-resource "aws_route_table" "public_rtb" {
-  vpc_id = aws_vpc.vpc.id
+# # Public subnet route table
+# resource "aws_route_table" "public_rtb" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.igw.id
+#   }
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-rtb-public-iac"
-  }
-}
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-rtb-public-iac"
+#   }
+# }
 
-# Private subnet route tables
-resource "aws_route_table" "private_rtb_1" {
-  vpc_id = aws_vpc.vpc.id
+# # Private subnet route tables
+# resource "aws_route_table" "private_rtb_1" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.private_subnet_1_nat_gateway.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.private_subnet_1_nat_gateway.id
+#   }
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-rtb-private1-ap-southeast-1a-iac"
-  }
-}
-resource "aws_route_table" "private_rtb_2" {
-  vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-rtb-private1-ap-southeast-1a-iac"
+#   }
+# }
+# resource "aws_route_table" "private_rtb_2" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.private_subnet_1_nat_gateway.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.private_subnet_1_nat_gateway.id
+#   }
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-rtb-private2-ap-southeast-1b-iac"
-  }
-}
-resource "aws_route_table" "private_rtb_3" {
-  vpc_id = aws_vpc.vpc.id
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-rtb-private2-ap-southeast-1b-iac"
+#   }
+# }
+# resource "aws_route_table" "private_rtb_3" {
+#   vpc_id = aws_vpc.vpc.id
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.private_subnet_1_nat_gateway.id
-  }
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_nat_gateway.private_subnet_1_nat_gateway.id
+#   }
 
-  tags = {
-    Name = "url-shortener-cluster-vpc-rtb-private3-ap-southeast-1c-iac"
-  }
-}
+#   tags = {
+#     Name = "url-shortener-cluster-vpc-rtb-private3-ap-southeast-1c-iac"
+#   }
+# }
 
-# Route associations
-resource "aws_route_table_association" "public_subnet_1_public_rtb_association" {
-  subnet_id      = aws_subnet.public_subnet_1.id
-  route_table_id = aws_route_table.public_rtb.id
-}
-resource "aws_route_table_association" "public_subnet_2_public_rtb_association" {
-  subnet_id      = aws_subnet.public_subnet_2.id
-  route_table_id = aws_route_table.public_rtb.id
-}
-resource "aws_route_table_association" "public_subnet_3_public_rtb_association" {
-  subnet_id      = aws_subnet.public_subnet_3.id
-  route_table_id = aws_route_table.public_rtb.id
-}
-resource "aws_route_table_association" "private_subnet_1_private_rtb_association" {
-  subnet_id      = aws_subnet.private_subnet_1.id
-  route_table_id = aws_route_table.private_rtb_1.id
-}
-resource "aws_route_table_association" "private_subnet_2_private_rtb_association" {
-  subnet_id      = aws_subnet.private_subnet_2.id
-  route_table_id = aws_route_table.private_rtb_2.id
-}
-resource "aws_route_table_association" "private_subnet_3_private_rtb_association" {
-  subnet_id      = aws_subnet.private_subnet_3.id
-  route_table_id = aws_route_table.private_rtb_3.id
-}
+# # Route associations
+# resource "aws_route_table_association" "public_subnet_1_public_rtb_association" {
+#   subnet_id      = aws_subnet.public_subnet_1.id
+#   route_table_id = aws_route_table.public_rtb.id
+# }
+# resource "aws_route_table_association" "public_subnet_2_public_rtb_association" {
+#   subnet_id      = aws_subnet.public_subnet_2.id
+#   route_table_id = aws_route_table.public_rtb.id
+# }
+# resource "aws_route_table_association" "public_subnet_3_public_rtb_association" {
+#   subnet_id      = aws_subnet.public_subnet_3.id
+#   route_table_id = aws_route_table.public_rtb.id
+# }
+# resource "aws_route_table_association" "private_subnet_1_private_rtb_association" {
+#   subnet_id      = aws_subnet.private_subnet_1.id
+#   route_table_id = aws_route_table.private_rtb_1.id
+# }
+# resource "aws_route_table_association" "private_subnet_2_private_rtb_association" {
+#   subnet_id      = aws_subnet.private_subnet_2.id
+#   route_table_id = aws_route_table.private_rtb_2.id
+# }
+# resource "aws_route_table_association" "private_subnet_3_private_rtb_association" {
+#   subnet_id      = aws_subnet.private_subnet_3.id
+#   route_table_id = aws_route_table.private_rtb_3.id
+# }
---
+++ terraform/modules/aws-vpc/outputs.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+  description = "ID of the vpc"
+  value = aws_vpc.vpc.id
+}
