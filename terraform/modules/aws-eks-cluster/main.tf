4412bee4816594c73f2003dce063efcece6f6ff0
Koh Jun Jie <koh.jun.jie@outlook.com>
2024-05-09 23:00:48 +0800

    dont use eks data resource

--- terraform/modules/aws-eks-cluster/main.tf
+++ terraform/modules/aws-eks-cluster/main.tf
@@ -115,14 +115,14 @@ resource "aws_eks_node_group" "node_group" {
 }
 
 data "tls_certificate" "cluster" {
-  url = data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+  url = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
 }
 
 # IAM OpenID Connect Provider for EKS cluster
 resource "aws_iam_openid_connect_provider" "cluster" {
   client_id_list = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.cluster.certificates.0.sha1_fingerprint]
-  url = data.aws_eks_cluster.cluster.identity.0.oidc.0.issuer
+  url = aws_eks_cluster.cluster.identity.0.oidc.0.issuer
 }
 
 data "aws_caller_identity" "current" {}
